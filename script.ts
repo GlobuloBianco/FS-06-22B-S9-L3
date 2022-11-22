e5d4c9dade20d740849b01ee5c50165420a75d1f
Globulo <109812198+GlobuloBianco@users.noreply.github.com>
2022-11-22 16:17:23 +0100

    Add files via upload

--- script.ts
+++ script.ts
@@ -9,29 +9,15 @@ let sezione = (T: string)=> {
 abstract class Attivita {
     nome: string;
     redditoAnnuoLordo: number;
-    constructor(_nome: string, _redditoAnnuoLordo: number) {
-        this.nome = _nome;
-        this.redditoAnnuoLordo = _redditoAnnuoLordo;
-    }
-
-    abstract getUtileTasse(): any;
-    abstract getTasseInps(): any;
-    abstract getTasseIrpef(): any;
-    abstract getRedditoAnnoNetto(): any;
-    abstract getRecap(): any;
-}
-
-sezione("||----Professionista----||"); //-------------------////-------------------////-------------------//
-
-class Professionista extends Attivita {
     codRedd: number;
     tasseIrpef: number;
     tasseInps: number;
-    constructor(_nome: string, _redditoAnnuoLordo: number) {
-        super(_nome, _redditoAnnuoLordo);
-        this.codRedd = 78;
-        this.tasseIrpef = 5;
-        this.tasseInps = 25.72;
+    constructor(_nome: string, _redditoAnnuoLordo: number, _codRedd: number, _tasseIrpef: number, _tasseInps: number) {
+        this.nome = _nome;
+        this.redditoAnnuoLordo = _redditoAnnuoLordo;
+        this.codRedd = _codRedd;
+        this.tasseIrpef = _tasseIrpef;
+        this.tasseInps = _tasseInps;
     }
 
     getUtileTasse(): any {
@@ -52,6 +38,14 @@ class Professionista extends Attivita {
 
     getRecap() {
         return `Gentile ${this.nome} il tuo stipendio annuo Lordo è di ${this.redditoAnnuoLordo}, togliendo le tasse inps ${this.getTasseInps()} e le tasse irpef ${this.getTasseIrpef()}, il tuo totale netto è di ${this.getRedditoAnnoNetto()}`
+    }
+}
+
+sezione("||----Professionista----||"); //-------------------////-------------------////-------------------//
+
+class Professionista extends Attivita {
+    constructor(_nome: string, _redditoAnnuoLordo: number) {
+        super(_nome, _redditoAnnuoLordo, 78, 5, 25.72);
     }
 }
 
@@ -61,34 +55,8 @@ console.log(newProfessionista.getRecap())
 sezione("||----Artigiano----||"); //-------------------////-------------------////-------------------//
 
 class Artigiano extends Attivita {
-    codRedd: number;
-    tasseIrpef: number;
-    tasseInps: number;
     constructor(_nome: string, _redditoAnnuoLordo: number) {
-        super(_nome, _redditoAnnuoLordo);
-        this.codRedd = 67;
-        this.tasseIrpef = 15;
-        this.tasseInps = 35;
-    }
-
-    getUtileTasse(): any {
-        return this.redditoAnnuoLordo * this.codRedd / 100;
-    }
-
-    getTasseInps(): any {
-        return this.getUtileTasse() * this.tasseInps / 100;
-    }
-
-    getTasseIrpef(): any {
-        return this.getUtileTasse() * this.tasseIrpef / 100;
-    }
-
-    getRedditoAnnoNetto() {
-        return this.redditoAnnuoLordo - (this.getTasseInps() + this.getTasseIrpef())
-    }
-
-    getRecap() {
-        return `Gentile ${this.nome} il tuo stipendio annuo Lordo è di ${this.redditoAnnuoLordo}, togliendo le tasse inps ${this.getTasseInps()} e le tasse irpef ${this.getTasseIrpef()}, il tuo totale netto è di ${this.getRedditoAnnoNetto()}`
+        super(_nome, _redditoAnnuoLordo, 67, 15, 35);
     }
 }
 
@@ -98,34 +66,8 @@ console.log(newArtigiano.getRecap())
 sezione("||----Commerciante----||"); //-------------------////-------------------////-------------------//
 
 class Commerciante extends Attivita {
-    codRedd: number;
-    tasseIrpef: number;
-    tasseInps: number;
     constructor(_nome: string, _redditoAnnuoLordo: number) {
-        super(_nome, _redditoAnnuoLordo);
-        this.codRedd = 40;
-        this.tasseIrpef = 15;
-        this.tasseInps = 35;
-    }
-
-    getUtileTasse(): any {
-        return this.redditoAnnuoLordo * this.codRedd / 100;
-    }
-
-    getTasseInps(): any {
-        return this.getUtileTasse() * this.tasseInps / 100;
-    }
-
-    getTasseIrpef(): any {
-        return this.getUtileTasse() * this.tasseIrpef / 100;
-    }
-
-    getRedditoAnnoNetto() {
-        return this.redditoAnnuoLordo - (this.getTasseInps() + this.getTasseIrpef())
-    }
-
-    getRecap() {
-        return `Gentile ${this.nome} il tuo stipendio annuo Lordo è di ${this.redditoAnnuoLordo}, togliendo le tasse inps ${this.getTasseInps()} e le tasse irpef ${this.getTasseIrpef()}, il tuo totale netto è di ${this.getRedditoAnnoNetto()}`
+        super(_nome, _redditoAnnuoLordo, 40, 15, 35);
     }
 }
 
